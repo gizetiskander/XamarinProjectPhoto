a56365ea692a7b4a8d81c34b0390c62db9c20f8d
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-20 08:28:08 +0300

    add db in project and add folders

---
+++ XamarinProjectPhoto/XamarinProjectPhoto/db/ISqlite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinProjectPhoto.db
+{
+    interface ISqlite
+    {
+        string GetDatabasePath(string filename);
+    }
+}
