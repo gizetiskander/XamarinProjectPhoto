1e60e90af84e334197ae0da9c1ab41857b77580f
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-20 09:24:42 +0300

    correct model

---
+++ XamarinProjectPhoto/XamarinProjectPhoto/Models/List.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinProjectPhoto.Models
+{
+    class List
+    {
+        public static List<Project> Projects = new List<Project>()
+        {
+
+        };
+    }
+}
