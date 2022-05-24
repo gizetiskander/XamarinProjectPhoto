624c0611162e9bd21b6ea77a9f4a4341e80cdf6c
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-24 08:55:55 +0300

    project page

---
+++ XamarinProjectPhoto/XamarinProjectPhoto/Views/TabbedPages/ContactsTabbedPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinProjectPhoto.Views.TabbedPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactsTabbedPage : TabbedPage
+    {
+        public ContactsTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
