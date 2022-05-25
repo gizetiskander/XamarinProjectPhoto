c3f5fcde538e7de791be40fff05a7c18db0aa142
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-25 12:21:07 +0300

    RegPage

--- XamarinProjectPhoto/XamarinProjectPhoto/Views/RegPage.xaml.cs
+++ XamarinProjectPhoto/XamarinProjectPhoto/Views/RegPage.xaml.cs
@@ -6,20 +6,33 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinProjectPhoto.Models;
 
 namespace XamarinProjectPhoto.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RegPage : ContentPage
     {
+        Client client = new Client();
         public RegPage()
         {
             InitializeComponent();
         }
 
-        private void btn_Reg_Clicked(object sender, EventArgs e)
+        private async void btn_Reg_Clicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (client.Password == entry_Password.Text)
+                { 
+                  App.Db.SaveClient(client);
+                 }
+                await Navigation.PushModalAsync(new NavigationPage(new MainPage()));
+            }
+            catch
+            {
+                await App.Current.MainPage.DisplayAlert("Уведомление", "Не удалось зарегистрироваться", "Ok");
+            }
         }
     }
 }
